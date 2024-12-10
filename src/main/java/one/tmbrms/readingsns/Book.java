9f35ca0cda02ec3c0243aa33bfa98659bfdaed58
TAMBARA Masahiko <tambara@jp.ibm.com>
2024-12-10 15:36:52 +0900

    thymeleaf.htmlを追加

---
+++ src/main/java/one/tmbrms/readingsns/Book.java
@@ -0,0 +1,15 @@
+package one.tmbrms.readingsns;
+
+public class Book {
+    public String name;
+    public String isbn;
+    
+    public Book(String name, String isbn){
+        this.name = name;
+        this.isbn = isbn;
+    }
+
+    public Book(String line) {
+        //TODO Auto-generated constructor stub
+    }
+}
---
+++ src/main/java/one/tmbrms/readingsns/GreetingController.java
@@ -0,0 +1,38 @@
+package one.tmbrms.readingsns;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/thymeleaf")
+    public String thymeleaf(Model model){
+
+        model.addAttribute("first","ファースト");
+        model.addAttribute("second","セカンド");
+        model.addAttribute("third","サード");
+
+        var books = new ArrayList<Book>();
+
+        var book = new Book("シャーロック★ホームズの凱旋", "9784120057342");
+        books.add(book);
+
+        book = new Book("入門 モダンLinux", "9784814400218");
+        books.add(book);
+
+        model.addAttribute("books", books);
+
+        return "thymeleaf";
+    }
+}
